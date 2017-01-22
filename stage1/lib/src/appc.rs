4c27a72bf8e7d40db6b53eb51fd7760b1e6caf28
Luca Bruno <lucab@debian.org>
2017-01-22 15:01:50 +0000

    lib/appc: support uid/gid from filestat

--- stage1/lib/src/appc.rs
+++ stage1/lib/src/appc.rs
@@ -4,6 +4,7 @@ use errors;
 include!(concat!(env!("OUT_DIR"), "/appc_pod.rs"));
 
 use std::{fs, path};
+use nix::sys::stat;
 
 pub fn resolve_uid(uid_field: &str, rootfs: &path::Path) -> errors::Result<u32> {
     let passwd = path::PathBuf::from(rootfs).join("etc").join("passwd");
@@ -13,13 +14,16 @@ pub fn resolve_uid(uid_field: &str, rootfs: &path::Path) -> errors::Result<u32> 
         }
     }
 
-    let tf = path::PathBuf::from(uid_field);
-    if tf.is_absolute() {
-        bail!("unimplemented");
-    }
-
     if let Ok(uid) = uid_field.parse::<u32>() {
         return Ok(uid);
+    }
+
+    let tf = path::PathBuf::from(uid_field);
+    if tf.is_absolute() {
+        let rel_path = try!(tf.strip_prefix("/"));
+        let abs_path = path::PathBuf::from(rootfs).join(rel_path);
+        let filestat = try!(stat::lstat(&abs_path));
+        return Ok(filestat.st_uid);
     }
 
     bail!("unable to determine uid");
@@ -33,13 +37,16 @@ pub fn resolve_gid(gid_field: &str, rootfs: &path::Path) -> errors::Result<u32> 
         }
     }
 
-    let tf = path::PathBuf::from(gid_field);
-    if tf.is_absolute() {
-        bail!("unimplemented");
-    }
-
     if let Ok(gid) = gid_field.parse::<u32>() {
         return Ok(gid);
+    }
+
+    let tf = path::PathBuf::from(gid_field);
+    if tf.is_absolute() {
+        let rel_path = try!(tf.strip_prefix("/"));
+        let abs_path = path::PathBuf::from(rootfs).join(rel_path);
+        let filestat = try!(stat::lstat(&abs_path));
+        return Ok(filestat.st_uid);
     }
 
     bail!("unable to determine gid");
--- stage1/lib/src/errors.rs
+++ stage1/lib/src/errors.rs
@@ -1,1 +1,8 @@
-error_chain!{}
+error_chain!{
+    foreign_links {
+        PathPrefix(::std::path::StripPrefixError);
+        Io(::std::io::Error);
+        Posix(::nix::Error);
+        Int(::std::num::ParseIntError);
+    }
+}
